42dca5148e7d27a7637b5ac539b592faeb9a0131
Toby Stayner <toby.stayner@gmail.com>
2019-11-10 13:11:28 +1300

    Created a template for messages

--- index.js
+++ index.js
@@ -26,14 +26,21 @@ client.on('message', message => {
   if (!message.content.startsWith(PREFIX)) return;
   const args = message.content.slice(PREFIX.length).split(/ +/);
   if (args[0] == 'about') {
-    const embeddedMessage = new Discord.RichEmbed()
-      .setColor('#115737')
-      .setAuthor('Examination Information')
+    const embeddedMessage = new richEmbedTemplate()
       .setTitle('About')
       .setDescription('A bot created to provide information about Victoria University of Wellington examination times for Discord.')
       .setFooter('Made by Toby', 'https://avatars0.githubusercontent.com/u/38903351'); // github profile picture
     message.channel.send(embeddedMessage);
   }
 });
+
+function richEmbedTemplate() {
+  const logo = new Discord.Attachment('./vuw-logo.png', 'vuw-logo.png');
+  var embed = new Discord.RichEmbed()
+    .attachFile(logo)
+    .setColor('#115737')
+    .setAuthor('Examination Information', 'attachment://vuw-logo.png', 'https://www.victoria.ac.nz/students/study/timetables')
+    return embed;
+}
 
 client.login(TOKEN);
