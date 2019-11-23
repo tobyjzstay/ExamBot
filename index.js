da2c68da6cb274fb042bed68a8101f031502cf7e
Toby Stayner <toby.stayner@gmail.com>
2019-11-24 10:05:39 +1300

    Commented code and improved formatting

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/**
+ * ExamBot
+ *
+ * A bot created to provide information about Victoria University of Wellington examination times for Discord.
+ * Made by Toby
+ */
 const Discord = require('discord.js');
 const fs = require("fs");
 const download = require('download-file');
@@ -8,17 +14,19 @@ var PREFIX = '!';
 var URL = 'https://www.victoria.ac.nz/__data/assets/excel_doc/0005/1766759/end-of-year-exam-timetable.xlsx';
 var FILENAME = "data.xlsx";
 
-var MAX_EMBED = 2000;
+var MAX_EMBED = 2000; // maximum characters allowed per embedded message
 
 const client = new Discord.Client();
 
+// retrive the token
 if (fs.existsSync('./auth.json')) {
   const { token } = require('./auth.json');
   TOKEN = token;
-} else {
+} else { // bot can't do much without a token...
   throw 'Missing auth.json file containing the token for the bot!';
 }
 
+// load configuration settings if there is a config file
 if (fs.existsSync('./config.json')) {
   const { prefix, url } = require('./config.json');
   PREFIX = prefix;
@@ -27,6 +35,10 @@ if (fs.existsSync('./config.json')) {
   console.log('Missing config.json file configuration settings for the bot! Default settings will be used.');
 }
 
+/**
+ * Retrives data from the source, keeping the data up to date.
+ * @return {boolean}
+ */
 function fetchData() {
   var options = { filename: FILENAME };
   download(URL, options, function(error){
@@ -37,8 +49,11 @@ function fetchData() {
   })
 }
 
-var data = {};
+var data = {}; // stores all the raw exam data
 
+/**
+ * Takes the data file and adds it to the object data array.
+ */
 function processData() {
   var stream = fs.createReadStream(FILENAME);
   var buffers = [];
@@ -62,22 +77,43 @@ function processData() {
   });
 }
 
+/**
+ * Returns a worksheet's cell value.
+ * @param {object}
+ * @param {string}
+ * @return {any}
+ */
 function getValue(worksheet, id) {
   var cell = worksheet[id];
   var value = (cell ? cell.v : undefined);
   return value;
 }
 
+/**
+ * Checks if the input is a valid exam.
+ * @param {string}
+ * @return {any}
+ */
 function parseExam(exam) {
   if (/^[a-zA-Z]{4}[0-9]{3}/.test(exam)) {
     return exam.slice(0, 7).toUpperCase();
   } else return undefined;
 }
 
+/**
+ * Returns the duration.
+ * @param {number}
+ * @return {number}
+ */
 function parseDuration(duration) {
   return duration;
 }
 
+/**
+ * Converts the raw input into a string formatted date.
+ * @param {number}
+ * @return {string}
+ */
 function parseDate(date) {
   var date = convertToDate(date);
   var day = date.getDate();
@@ -87,10 +123,23 @@ function parseDate(date) {
   return `${day}/${month}/${date.getFullYear()}`
 }
 
+/**
+ * Converts the raw input into a Date object.
+ * The input is a serial number which represents how many days past 1 Janurary 1900.
+ * Date objects can be created with serial numbers, but they start from 1 Janurary 1970.
+ * @param {number}
+ * @return {Date}
+ */
 function convertToDate(date) {
-  return new Date((date-25569)*86400000);
+  return new Date((date-25569)*86400000); // 25569 accounts for serial number shift.
 }
 
+/**
+ * Converts the raw input into a string formatted start time.
+ * The input is a decimal value between 0 and 1, representing the amount of time past midnight of the day.
+ * @param {number}
+ * @return {string}
+ */
 function parseStart(start) {
   var hour = Math.floor(start*24);
   var minute = Math.floor(start*24%1*60);
@@ -99,18 +148,33 @@ function parseStart(start) {
   return `${hour%12}:${minute} ${meridiem}`
 }
 
+/**
+ * Returns the rooms.
+ * @param {string}
+ * @return {string}
+ */
 function parseRooms(rooms) {
-  if (true) {
-    return rooms;
-  } else return undefined;
+  return rooms;
 }
 
+/**
+ * Checks if the input is a valid role.
+ * @param {string}
+ * @return {any}
+ */
 function parseRole(role) {
   if (/^[a-zA-Z]{4}-[0-9]{3}/.test(role)) {
     return role.slice(0, 4).toUpperCase() + role.slice(5, 8);
   } else return undefined;
 }
 
+/**
+ * Generates a formatted string containing the exam data.
+ * @param {object}
+ * @param {object}
+ * @param {boolean}
+ * @return {string}
+ */
 function formatExams(message, exams, displayErrors) {
   var examData = '';
   for (var i = 0; i < exams.length; i++) {
@@ -125,85 +189,93 @@ function formatExams(message, exams, displayErrors) {
   return examData;
 }
 
+/**
+ * Called once when the bot is executed on startup.
+ */
 client.on('ready', () => {
   client.user.setStatus('online');
   client.user.setActivity(`${PREFIX}help`);
   console.log(`${client.user.tag} connected at ${new Date()}\n`);
 });
 
+/**
+ * Called when a message is sent in the server.
+ */
 client.on('message', message => {
-  if (!message.content.startsWith(PREFIX)) return;
-  const args = message.content.slice(PREFIX.length).split(/ +/);
-  if (args[0] == 'about') {
+  if (!message.content.startsWith(PREFIX)) return; // the message must start with the prefix
+  const args = message.content.slice(PREFIX.length).split(/ +/); // slice message into arguments
+  if (args[0] == 'about') { // information about the bot
     const embeddedMessage = new richEmbedTemplate()
       .setTitle('About')
       .setDescription('A bot created to provide information about Victoria University of Wellington examination times for Discord.')
       .setFooter('Made by Toby', 'https://avatars0.githubusercontent.com/u/38903351'); // github profile picture
     message.channel.send(embeddedMessage);
-  } else if (args[0] == 'status') {
+  } else if (args[0] == 'status') { // statistics about the bot
     const embeddedMessage = new richEmbedTemplate()
       .setTitle('Status')
       .setDescription(`The bot has been running since ${client.readyAt} (${formatTime(client.uptime)})`);
     message.channel.send(embeddedMessage);
-  } else if (args[0] == 'fetch') {
+  } else if (args[0] == 'fetch') { // retrives the data from the source
     if (fetchData()) message.reply("an error has occurred attempting to fetch the exam data.");
     else message.reply("successfully fetched the exam data.");
   } else if (args[0] == 'exam') {
-    if (args.length < 2) {
+    if (args.length < 2) { // missing exam course
       message.reply(`missing arguments. Valid arguments: \`!exam <course> [course ...]\` e.g. \`!exam comp102\` \`!exam cgra-151 comp-103 engr-123\``);
       return;
     }
-
+    // find exam courses
     var exams = [args.length-1];
     for (var i = 1; i < args.length; i++) {
       exams[i-1] = args[i];
     }
-    var examData = formatExams(message, exams, true);
-    if (examData.length > 0) {
-    const embeddedMessage = new richEmbedTemplate()
-      .setTitle('Exam Times')
-      .setDescription(`\`\`\`${examData}\`\`\``)
-      .addField('\u200b', 'To find out your room, login into [Student Records](https://student-records.vuw.ac.nz).')
-    message.reply(embeddedMessage);
-    }
+    console.log(typeof exams);
+    var examData = formatExams(message, exams, true); // get the formatted data
+    if (examData.length > 0) { // generate the embedded message
+      const embeddedMessage = new richEmbedTemplate()
+        .setTitle('Exam Times')
+        .setDescription(`\`\`\`${examData}\`\`\``)
+        .addField('\u200b', 'To find out your room, login into [Student Records](https://student-records.vuw.ac.nz).');
+        message.reply(embeddedMessage);
+      }
   } else if (args[0] == 'exams') {
+    // find the exam courses of the user by checking their roles
     var exams = new Array();
     message.member.roles.forEach(function(value) {
       var exam = parseRole(value.name);
       if (exam) exams.push(exam);
     });
-    var examData = formatExams(message, exams, false);
-
-    if (examData.length > 0) {
+    var examData = formatExams(message, exams, false); // get the formatted data
+    if (examData.length > 0) { // generate the embedded message
       const embeddedMessage = new richEmbedTemplate()
         .setTitle('Exam Times')
         .setDescription(`\`\`\`${examData}\`\`\``)
-        .addField('\u200b', 'To find out your room, login into [Student Records](https://student-records.vuw.ac.nz).')
+        .addField('\u200b', 'To find out your room, login into [Student Records](https://student-records.vuw.ac.nz).');
       message.reply(embeddedMessage);
-    } else message.reply('couldn\'t find exam data for your course roles for the current trimister.');
-  } else if (args[0] == 'list') {
+    } else message.reply('couldn\'t find exam data for your course roles for the current trimister.'); // none of the user courses were valid
+  } else if (args[0] == 'list') { // display all the exam data
     var exams = Object.keys(data);
-    exams.sort(function(a, b) {
+    exams.sort(function(a, b) { // sort by alphabetical exam course
       if(a < b) { return -1; }
       if(a > b) { return 1; }
       return 0;
     })
-    var examData = formatExams(message, exams, true);
-
+    var examData = formatExams(message, exams, true); // get the formatted data
+    // split the exam data into messages - based on the maximum embedded character limit
     var examDataMessages = new Array();
     var examDataList = examData.split("\n");
     var currentMessage = "";
     for (var i = 0; i < examDataList.length; i++) {
-      if (currentMessage.length + examDataList[i].length > MAX_EMBED) {
-        examDataMessages.push(currentMessage);
+      if (currentMessage.length + examDataList[i].length > MAX_EMBED) { // adding the exam data will overflow the character count
+        examDataMessages.push(currentMessage); // add the message to the array
         currentMessage = examDataList[i];
-      } else {
+      } else { // add the exam data
         currentMessage += examDataList[i] + "\n";
       }
     }
+    // generate the embedded messages
     for (var i = 0; i < examDataMessages.length; i++) {
       var embeddedMessage = null;
-      if (i == 0) {
+      if (i == 0) { // first message
         embeddedMessage = new richEmbedTemplate()
           .setTitle('Exam Times')
           .setDescription(`\`\`\`${examDataMessages[i]}\`\`\``)
@@ -213,29 +285,42 @@ client.on('message', message => {
           .setDescription(`\`\`\`${examDataMessages[i]}\`\`\``)
           .setFooter(`Page ${i+1} of ${examDataMessages.length}`);
       }
-      if (i == examDataMessages.length-1) embeddedMessage.addField('\u200b', 'To find out your room, login into [Student Records](https://student-records.vuw.ac.nz).');
-
-      if (i == 0) message.reply(embeddedMessage);
+      if (i == examDataMessages.length-1) embeddedMessage.addField('\u200b', 'To find out your room, login into [Student Records](https://student-records.vuw.ac.nz).'); // last message
+      // how the bot should send the message
+      if (i == 0) message.reply(embeddedMessage); // first message
       else message.channel.send(embeddedMessage);
     }
   }
 });
 
+/**
+ * Generates an embedded message template, includuing the bot logo.
+ * @return {object}
+ */
 function richEmbedTemplate() {
   const logo = new Discord.Attachment('./vuw-logo.png', 'vuw-logo.png');
   var embed = new Discord.RichEmbed()
     .attachFile(logo)
     .setColor('#115737')
-    .setAuthor('Examination Information', 'attachment://vuw-logo.png', 'https://www.victoria.ac.nz/students/study/timetables')
+    .setAuthor('Examination Information', 'attachment://vuw-logo.png', 'https://www.victoria.ac.nz/students/study/timetables');
     return embed;
 }
 
+/**
+ * Generates an embedded message template which is basic.
+ * @return {object}
+ */
 function blankEmbedTemplate() {
   var embed = new Discord.RichEmbed()
     .setColor('#115737')
     return embed;
 }
 
+/**
+ * Converts the raw input into a string formatted time.
+ * @param {number}
+ * @return {string}
+ */
 function formatTime(milliseconds) {
   let totalSeconds = (milliseconds / 1000);
   let days = Math.floor(totalSeconds / 86400);
@@ -246,6 +331,7 @@ function formatTime(milliseconds) {
   return `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
 }
 
+// update and process the data before running the client
 fetchData();
 processData();
 
